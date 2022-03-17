c64e8b409f924deb3eba4a187fb0828343374076
thiagobrsouza-edu <thiago.glespaul@gmail.com>
2022-03-17 17:02:04 -0300

    Entity and DTO

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsEmpty, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmpty()
+  id: number;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  profile: string;
+}
--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  name?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email?: string;
+
+  @IsNotEmpty()
+  password?: string;
+
+  @IsNotEmpty()
+  profile?: string;
+}
